aa5c4521326455e9af312b37ccf6bf670acfec9c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-06-14 11:10:57 +0700

    1234

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
-type Props = {};
-
-export default function index({}: Props) {
-  return <div>Login</div>;
+export default function BasicButtons() {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
 }
