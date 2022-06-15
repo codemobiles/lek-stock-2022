65669d52ef00d2f71ef6c6083e41067a371bc0ec
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-06-15 10:15:35 +0700

    1234

---
+++ services/serverService.ts
@@ -0,0 +1,33 @@
+import { SignUp, SignIn, GetSession } from "@/models/auth.model";
+// import { ProductData } from "@/models/product.model";
+import { UserData } from "@/models/user.model";
+import httpClient from "@/utils/httpClient";
+import axios from "axios";
+
+type signProps = {
+  username: string;
+  password: string;
+};
+
+export const signUp = async (user: signProps): Promise<SignUp> => {
+  const response = await httpClient.post<SignUp>("/authen/register", user);
+  return response.data;
+};
+
+export const signIn = async (user: signProps): Promise<SignIn> => {
+  const { data: response } = await httpClient.post<SignIn>(
+    `/auth/signin`,
+    user,
+    {
+      baseURL: process.env.NEXT_PUBLIC_BASE_URL_LOCAL_API,
+    }
+  );
+  return response;
+};
+
+export async function signOut() {
+  const response = await httpClient.get(`/auth/signout`, {
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL_LOCAL_API,
+  });
+  return response.data;
+}
